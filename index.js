fc16fd29f3dc8cf4095a2e97a7843f1cbc0e5740
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-10-27 08:18:54 +0600

    gitignore add

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // cors initialized
 app.use(cors());
+
 const courses = require('./Data/Courses.json');
 
 
@@ -17,6 +17,13 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const courseDetails = courses.find(course => course.id === id);
+    res.send(courseDetails);
+})
+
 
 app.listen(port, () => {
     console.log(port);
